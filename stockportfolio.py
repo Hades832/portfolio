e1e63545834e6b0daacf8de172f2e5e1e4f366eb
Hades832 <86631824+Hades832@users.noreply.github.com>
2025-04-18 21:48:01 -0400

    Update stockportfolio.py

--- stockportfolio.py
+++ stockportfolio.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.api as sm
 import yfinance as yf
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import streamlit as st
-from datetime import datetime
+import math
+import pylab
 
 # Initialize session state
 if 'portfolio_data' not in st.session_state:
